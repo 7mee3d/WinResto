11a73e2fd1a45e289ba68b9b8be88776e1ed1f04
Ahmed Jehad <alanturing404@gmail.com>
2025-09-10 16:49:37 +0300

    Add project files.

---
+++ Simple Project - Food Menu/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Simple_Project___Food_Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Sienna;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(1197, 724);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 47);
+            button1.TabIndex = 0;
+            button1.Text = "Order Now";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources._1;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(1512, 847);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Food Restaurant";
+            Load += Form1_Load;
+            Move += Form1_Move;
+            Resize += Form1_Resize;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ Simple Project - Food Menu/Form1.cs
@@ -0,0 +1,35 @@
+namespace Simple_Project___Food_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setFormSizeAndMoveFixed()
+        {
+            this.Size = new Size(1528, 886);
+            int xWidth = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2; 
+            int yHeight = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+
+            this.Location = new Point(xWidth, yHeight); 
+
+
+        }
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            setFormSizeAndMoveFixed();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            setFormSizeAndMoveFixed();
+        }
+    }
+}
